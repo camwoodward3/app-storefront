6edc2a0ea6321c65d74e5eba477ec16337097236
Cameron Woodward <woo23017@byui.edu>
2026-03-12 11:13:22 -0600

    Ready for grading

---
+++ db/src/main/java/edu/byui/apj/storefront/db/repository/UserProfileRepository.java
@@ -0,0 +1,6 @@
+package edu.byui.apj.storefront.db.repository;
+import edu.byui.apj.storefront.db.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+}
