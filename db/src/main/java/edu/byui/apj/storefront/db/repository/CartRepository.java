3ec57181467a0f806cbd67880dff2a28f1f457b4
Cameron Woodward <woo23017@byui.edu>
2026-03-20 15:49:56 -0600

    Ready for grading

--- db/src/main/java/edu/byui/apj/storefront/db/repository/CartRepository.java
+++ db/src/main/java/edu/byui/apj/storefront/db/repository/CartRepository.java
@@ -3,5 +3,12 @@ package edu.byui.apj.storefront.db.repository;
 import edu.byui.apj.storefront.db.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    /**
+     * Find carts created before the given cutoff time (for cleanup of expired carts).
+     */
+    List<Cart> findByCreatedAtBefore(Instant cutoff);
 }
---
+++ work/src/test/java/edu.byui.apj.storefront.work/WorkApplicationTests.java
@@ -0,0 +1,11 @@
+package edu.byui.apj.storefront.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WorkApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+}
